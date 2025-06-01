fac69dcb2434080d2b428a3fdd3b60ef2e74c867
Vinithsaireddy <148205482+Vinithsaireddy@users.noreply.github.com>
2025-06-01 13:35:28 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 mongoose.connect("mongodb+srv://gvinith2005:cihXKejEeEsCoTgN@expense-tracker.ee3if.mongodb.net/")
     .then(() => console.log("Connected to MONGO_DB"))
     .catch(err => console.error('DB error', err));
+
+
+app.use(cors({
+  origin: 'https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--55edb8f4.local-credentialless.webcontainer-api.io' || "http://localhost:5173",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
