5def7b1c7856976b8b57be1d388d4154dad23e42
G Vinith Sai Reddy <148205482+Vinithsaireddy@users.noreply.github.com>
2025-06-01 22:34:40 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,4 @@ const authenticateToken = require('../middleware/authMiddleware');
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get('/verify', authenticateToken, (req, res) => {
-  res.json({ user: req.user }); // make sure `req.user` is set by the middleware
-});
 module.exports = router;
